fcb0a353b5eff85a9aea5bf1f391fe6b0930808a
Mansi Bambal <165362707+MahiiBamba@users.noreply.github.com>
2025-12-16 15:54:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def predict():
     return render_template("home.html", result=result)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
+
+
